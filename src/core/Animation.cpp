bf0beb9456787dfee0a3ab9481d20c68755be6bd
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-03-21 14:45:35 +0100

    Implémentation: fonctions génératrices d'Animation

--- src/core/Animation.cpp
+++ src/core/Animation.cpp
@@ -1,25 +1,52 @@
 #include "Animation.h"
 
-LinearAnimation create_linear_animation(const Entity& object, Vec2f start_point, Vec2f movement, float speed)
+LinearAnimation create_linear_animation(Entity* object, Vec2f start_point, Vec2f movement, float speed)
 {
-    exit(1);
-    //TODO Implémenter la fonction
+    LinearAnimation anim;
+    anim.object = object;
+    anim.type   = LINEAR;
+
+    anim.start_point    = start_point;
+    anim.movement       = movement;
+    anim.speed          = speed;
+
+    return anim;
 }
 
-RotationAnimation create_rotation_animation(const Entity& object, float start_angle, float rotation_speed);
+RotationAnimation create_rotation_animation(Entity* object, float start_angle, float rotation_speed)
 {
-    exit(1);
-    //TODO Implémenter la fonction
+    RotationAnimation anim;
+    anim.object = object;
+    anim.type   = ROTATION;
+
+    anim.start_angle    = start_angle;
+    anim.rotation_speed = rotation_speed;
+
+    return anim;
 }
 
-BouncyAnimation create_bouncy_animation(const Entity& object, float rebound_height, float rebound_width, int max_bounces)
+BouncyAnimation create_bouncy_animation(Entity* object, float rebound_height, float rebound_width, int max_bounces)
 {
-    exit(1);
-    //TODO Implémenter la fonction
+    BouncyAnimation anim;
+    anim.object = object;
+    anim.type   = BOUNCY;
+
+    anim.height         = rebound_height;
+    anim.width          = rebound_width;
+    anim.max_bounces    = max_bounces;
+    anim.bounces = 0;
+
+    return anim;
 }
 
-PhysicAnimation create_physic_animation(const Entity& object, Vec2f acceleration, float bounciness)
+PhysicAnimation create_physic_animation(Entity* object, Vec2f acceleration, float bounciness)
 {
-    exit(1);
-    //TODO Implémenter la fonction
+    PhysicAnimation anim;
+    anim.object = object;
+    anim.type   = PHYSIC;
+
+    anim.acc        = acceleration;
+    anim.bounciness = bounciness;
+
+    return anim;
 }
