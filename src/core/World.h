e879b27c4cdff8b9a7073dd021d3ea4457218ba8
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-03-21 13:54:02 +0100

    Ajout d'un include et d'une suggestion

--- src/core/World.h
+++ src/core/World.h
@@ -1,11 +1,14 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+#include <vector>
+
 #include "Player.h"
 #include "Animation.h"
 #include "Platform.h"
 
 /// TODO Implémenter World en dernier lieu
+//? Placer Segment dans un fichier séparé serait une bonne idée
 
 
 
