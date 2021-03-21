cd25024ebcc0e9626d84afa2208255d422fea34a
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-03-21 14:33:30 +0100

    Implémentation de Entity

--- src/core/Entity.cpp
+++ src/core/Entity.cpp
@@ -9,6 +9,10 @@ angle(0)
 
 Entity::Entity(Vec2f pos, float width, float height, float angle)
 {
+    setPosition(pos);
+    setWidth(width);
+    setHeight(height);
+    setAngle(angle);
 }
 
 Entity::~Entity()
@@ -17,52 +21,65 @@ Entity::~Entity()
 
 Vec2f   Entity::getPosition() const
 {
+    return pos;
 }
 
 void    Entity::setPosition(Vec2f newpos)
 {
+    pos = newpos;
 }
 
 Vec2f   Entity::getVelocity() const
 {
+    return vel;
 }
 
 void    Entity::setVelocity(Vec2f newvel)
 {
+    vel = newvel;
 }
 
 float   Entity::getAngle() const
 {
+    return angle;
 }
 
 void    Entity::setAngle(float newangle)
 {
+    angle = newangle;
 }
 
 float   Entity::getWidth() const
 {
+    return width;
 }
 
 void    Entity::setWidth(float newwidth)
 {
+    width = newwidth;
 }
 
 float   Entity::getHeight() const
 {
+    return height;
 }
 
 void    Entity::setHeight(float newheight)
 {
+    height = newheight;
 }
 
 void    Entity::applyForce(Vec2f force)
 {
+    vel += force;
 }
 
 void    Entity::move(Vec2f offset)
 {
+    pos += offset;
 }
 
 void    Entity::rotate(float angle)
 {
+    this->angle += angle;
 }
