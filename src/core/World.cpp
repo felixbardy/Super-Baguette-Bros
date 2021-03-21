c4e0a980bc41c07674587e9f56443a520507d8e4
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-03-21 18:37:31 +0100

    Implémentation timide de Segment et World

--- src/core/World.cpp
+++ src/core/World.cpp
@@ -1,1 +1,17 @@
 #include "World.h"
+
+Segment::Segment(Platform* platforms, Animation* animations)
+{
+}
+
+Segment::~Segment()
+{
+}
+
+World::World(/* args */)
+{
+}
+
+World::~World()
+{
+}
--- src/core/World.h
+++ src/core/World.h
@@ -10,6 +10,52 @@
 /// TODO Implémenter World en dernier lieu
 //? Placer Segment dans un fichier séparé serait une bonne idée
 
+class Segment
+{
+private:
+    //? Ajouter un ID de Segment répercuté sur les Entités / Animations permettrait de savoir quoi supprimer pour décharger le Segment
+    //? static int ID;
+    /// Tableau des plateformes associées au segment
+    Platform* platforms;
+    /// Nombre de plateformes associées au segment
+    int nPlatforms;
+    /// Tableau des animations associées au segment
+    Animation* animations;
+    /// Nombre d'animations associées au segment
+    int nAnimations;
+public:
+    // TODO Définir un constructeur par flux de fichier pour charger le segment depuis un fichier de niveau
+    /// Constructeur par données
+    Segment(Platform* platforms, Animation* animations);
+    /// Destructeur 
+    ~Segment();
+};
+
+class World
+{
+private:
+    /// Le joueur
+    Player player;
+
+    Segment* segments;
+    int nSegments;
+
+    // TODO Décider de la meilleure façon de stocker les plateformes
+    //! Les plateformes doivent avoir des adresses fixes
+    Platform* platforms;
+    int nPlatforms;
+
+    //? Ajouter une liste pour d'autres entités
+
+    //! A chaque suppression d'animation de ce vector: FAIRE UN delete SUR LE POINTEUR
+    /// Tableau dynamique stockant les pointeurs vers les Animations
+    std::vector<Animation*> animations;
+
+public:
+    World(/* args */);
+    ~World();
+};
+
 
 
 #endif //WORLD_H
