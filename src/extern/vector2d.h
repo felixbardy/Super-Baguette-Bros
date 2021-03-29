e47ad89844152ed38fe7362cf8920a2cfe992f48
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-03-29 10:27:01 +0200

    Ajout d'une fonction de calcul d'intersection Ajoutée dans la librairie de vecteurs

--- src/extern/vector2d.h
+++ src/extern/vector2d.h
@@ -226,5 +226,28 @@ typedef Vector2D <float> Vec2f;
 //! Vec2d would be 2D vector with double components
 typedef Vector2D <double> Vec2d;
 
+template <class RealType>
+Vector2D<RealType> intersection(Vector2D<RealType> A, Vector2D<RealType> B, Vector2D<RealType> C, Vector2D<RealType> D, bool& intersects = false)
+{
+     // Line AB represented as a1x + b1y = c1
+     double a = B.y - A.y;
+     double b = A.x - B.x;
+     double c = a*(A.x) + b*(A.y);
+     // Line CD represented as a2x + b2y = c2
+     double a1 = D.y - C.y;
+     double b1 = C.x - D.x;
+     double c1 = a1*(C.x)+ b1*(C.y);
+     double det = a*b1 - a1*b;
+     if (det == 0) {
+          intersects = false;
+          return Vector2D<RealType>();
+     } else {
+          intersects = true;
+          double x = (b1*c - b*c1)/det;
+          double y = (a*c1 - a1*c)/det;
+          return {x,y};
+     }
+}
+
 #endif
 
