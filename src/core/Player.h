eb08f2998145e293a597347857760f131a876be7
BEKKER VICTORIA p1914415 <victoria.bekker@etu.univ-lyon1.fr>
2021-03-29 15:26:30 +0000

    Ajout des getter/setters et documentation

--- src/core/Player.h
+++ src/core/Player.h
@@ -3,19 +3,48 @@
 
 #include "Entity.h"
 
-
 /// TODO Fixer clairement à quoi ressemblent les contrôles du joueur avant de l'implémenter
 
 class Player : public Entity
 {
 private:
+
+    /// Indique si le joueur est dans l'air
+    bool in_air;
+
+    /// Indique si le joueur est en train de sauter
+    bool jumping;
+
     /// Stocke les masques passés comme input
     uint16_t current_input;
+
 public:
+
     /// Constructeur par défaut de Player
     Player();
     /// Destructeur de Player
     ~Player();
+
+    /** \brief Modifie la position du joueur lors d'un deplacement à gauche
+     * \param pos La position initiale du joueur
+     **/
+    void moveleft(Vec2f& pos);
+
+    /** \brief Modifie la position du joueur lors d'un deplacement à droite
+     * \param pos La position initiale du joueur
+     **/
+    void moveright(Vec2f& pos);
+
+    /** \brief Modifie la position du joueur lors d'un saut
+     * \param pos La position initiale du joueur
+     **/
+    void jump(Vec2f& pos);
+
+    /// Retourne la valeur de jumping
+    bool isJumping();
+
+    /// Retourne la valeur de in_air
+    bool isInAir();
 
     /** \brief Permet d'activer les inputs voulus du joueur
      * \param mask Le masque représentant les inputs à activer
@@ -46,6 +75,7 @@ public:
     static const uint16_t LEFT     = 1 << 3;
     static const uint16_t RIGHT    = 1 << 4;
     // Il reste de la place pour encore 11 inputs (ex: attaque)
+
 };
 
 
