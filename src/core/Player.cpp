dfb205ffe05415de946704cb464de56ef04d1562
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-03-29 16:43:18 +0200

    Ajout de la gestion des inputs de Player Utilisation de masques stockés dans PlayerInput

--- src/core/Player.cpp
+++ src/core/Player.cpp
@@ -1,11 +1,32 @@
 #include "Player.h"
 
 Player::Player(/* args */)
+: current_input(0)
 {
 }
 
 Player::~Player()
 {
+}
+
+void Player::addInput(uint16_t mask)
+{
+    current_input |= mask;
+}
+
+void Player::clearInput(uint16_t mask)
+{
+    current_input &= ~mask;
+}
+
+bool Player::checkInput(uint16_t mask)
+{
+    return (bool)(current_input & mask);
+}
+
+void Player::clearAllInputs()
+{
+    current_input = 0;
 }
 
 void Player::testRegression()
--- src/core/Player.h
+++ src/core/Player.h
@@ -3,15 +3,34 @@
 
 #include "Entity.h"
 
+namespace PlayerInput
+{
+    // Masques
+    const uint16_t JUMP     = 1 << 0; // = 0x0...01
+    const uint16_t DOWN     = 1 << 1; // = 0x0...10
+    const uint16_t UP       = 1 << 2; // etc...
+    const uint16_t LEFT     = 1 << 3;
+    const uint16_t RIGHT    = 1 << 4;
+    // Il reste de la place pour encore 11 inputs (ex: attaque)
+
+};
+
+
 /// TODO Fixer clairement à quoi ressemblent les contrôles du joueur avant de l'implémenter
 
 class Player : public Entity
 {
 private:
-    /* data */
+    /// Stocke les masques passés comme input
+    uint16_t current_input;
 public:
     Player(/* args */);
     ~Player();
+    void addInput(uint16_t mask);
+    void clearInput(uint16_t mask);
+    bool checkInput(uint16_t mask);
+    void clearAllInputs();
+    
     void testRegression();
 };
 
