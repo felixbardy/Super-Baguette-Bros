ac2128a959647dafc8808411231ee440f6d1b7ae
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-03-29 16:56:57 +0200

    Déplacement des masques dans Player pour une structure plus compréhensible

--- src/core/Player.h
+++ src/core/Player.h
@@ -3,18 +3,6 @@
 
 #include "Entity.h"
 
-namespace PlayerInput
-{
-    // Masques
-    const uint16_t JUMP     = 1 << 0; // = 0x0...01
-    const uint16_t DOWN     = 1 << 1; // = 0x0...10
-    const uint16_t UP       = 1 << 2; // etc...
-    const uint16_t LEFT     = 1 << 3;
-    const uint16_t RIGHT    = 1 << 4;
-    // Il reste de la place pour encore 11 inputs (ex: attaque)
-
-};
-
 
 /// TODO Fixer clairement à quoi ressemblent les contrôles du joueur avant de l'implémenter
 
@@ -30,8 +18,17 @@ public:
     void clearInput(uint16_t mask);
     bool checkInput(uint16_t mask);
     void clearAllInputs();
-    
+
     void testRegression();
+
+    // Masques d'input
+    // Masques
+    static const uint16_t JUMP     = 1 << 0; // = 0x0...01
+    static const uint16_t DOWN     = 1 << 1; // = 0x0...10
+    static const uint16_t UP       = 1 << 2; // etc...
+    static const uint16_t LEFT     = 1 << 3;
+    static const uint16_t RIGHT    = 1 << 4;
+    // Il reste de la place pour encore 11 inputs (ex: attaque)
 };
 
 
