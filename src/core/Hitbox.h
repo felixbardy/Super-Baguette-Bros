7d1e30af78591263c6d40cdfc9e9bb002fe861d3
BARDY FELIX p1925107 <felix.bardy@etu.univ-lyon1.fr>
2021-04-22 18:45:08 +0200

    Documentation de Hitbox et ajout d'images à la doc

--- src/core/Hitbox.h
+++ src/core/Hitbox.h
@@ -1,5 +1,21 @@
 #include "extern/vector2d.h"
 
+/** \class Hitbox Hitbox.h "src/Hitbox.h"
+ * \brief représente une boite de collision
+ * 
+ * Exemple de Hitbox::upper:<br>
+ * scale est situé sur une échelle continue de -1 à 1:<br>
+ * 1    -> Toute la partie haute<br>
+ * 0.5  -> La moitié de la partie haute en partant du haut<br>
+ * 0    -> Renvoie une erreur<br>
+ * -0.5 -> La moitié de la partie haute en partant du milieu<br>
+ * -1   -> Toute la partie haute<br>
+ * Autres exemples en image:<br>
+ * \image html hitbox1.jpg width=25%
+ * \image html hitbox2.jpg width=25%
+ * \image html hitbox3.jpg width=25%
+ * 
+ **/
 class Hitbox
 {
 private:
@@ -20,31 +36,83 @@ public:
     /// Destructeur de Hitbox
     ~Hitbox();
 
+
     /// Renvoie la position de la Hitbox
     Vec2f getPos() const;
+
     /// Affecte la nouvelle position à la Hitbox
     void setPos(Vec2f newpos);
 
+
     /** \brief Renvoie les dimensions de la Hitbox
-     * \param width pointeur vers le float à affecter
-     * 
+     * \param width le float auquel affecter la largeur
+     * \param heigth le float auquel affecter la hauteur
      **/
     void getDimensions(float* width, float* height) const;
+
+    /** \brief Remplace la largeur de la Hitbox
+     * \param width la largeur à affecter
+     **/
     void setWidth(float width);
+
+    /** \brief Remplace la hauteur de la Hitbox
+     * \param width la hauteur à affecter
+     **/
     void setHeight(float height);
 
+
+    /** \brief Renvoie true si les 2 hitboxs sont superposées, faux sinon
+     * \param h1 la 1ère Hitbox
+     * \param h2 la 2ème Hitbox
+     **/
     static bool overlaping(const Hitbox& h1, const Hitbox& h2);
 
-    Hitbox upper(float scale) const;
-    Hitbox lower(float scale) const;
-    Hitbox right_inner(float scale) const;
-    Hitbox left_inner(float scale) const;
 
-    Hitbox top(float scale) const;
-    Hitbox bottom(float scale) const;
-    Hitbox right_outer(float scale) const;
-    Hitbox left_outer(float scale) const;
+    /** \brief Renvoie la partie intérieure haute de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox upper(float scale = 1) const;
 
+    /** \brief Renvoie la partie intérieure basse de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox lower(float scale = 1) const;
+
+    /** \brief Renvoie la partie intérieure droite de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox right_inner(float scale = 1) const;
+
+    /** \brief Renvoie la partie intérieure gauche de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox left_inner(float scale = 1) const;
+
+
+    /** \brief Renvoie la partie extérieure haute de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox top(float scale = 0.25) const;
+
+    /** \brief Renvoie la partie extérieure basse de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox bottom(float scale = 0.25) const;
+
+    /** \brief Renvoie la partie extérieure droite de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox right_outer(float scale = 0.25) const;
+
+    /** \brief Renvoie la partie extérieure gauche de la Hitbox (voir doc. de la classe)
+     * \param scale paramètre de modulation
+     **/
+    Hitbox left_outer(float scale = 0.25) const;
+
+
+    /** \brief Renvoie la Hitbox redimensionnée (voir doc. de la classe)
+     * \param scale facteur de redimensionnement
+     **/
     Hitbox resized(float scale) const;
 
 };
