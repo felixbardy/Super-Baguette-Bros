c14084b74fd63d213a32d3096c76b5105b838fd1
BEKKER VICTORIA p1914415 <victoria.bekker@etu.univ-lyon1.fr>
2021-05-08 13:56:45 +0000

    Ajout du test de regression et un constructeur par defaut

--- src/core/Hitbox.cpp
+++ src/core/Hitbox.cpp
@@ -3,6 +3,13 @@
 #include <iostream>
 #include <cassert>
 
+Hitbox::Hitbox()
+: width(0), height(0)
+{
+    pos.x = 0;
+    pos.y = 0;
+}
+
 Hitbox::Hitbox(Vec2f pos, float width, float height)
 : pos(pos), width(width), height(height)
 {
@@ -204,4 +211,114 @@ Hitbox Hitbox::resized(float scale) const
         width * scale,
         height * scale
     );
+}
+
+void Hitbox::testRegression()
+{
+    cout << "Hitbox: constructeur par défaut... ";
+    Hitbox h;
+    assert(h.pos.x == 0 && h.pos.y == 0);
+    assert(h.width == 0 && h.height == 0);
+    cout << "OK" << endl;
+
+
+    cout << "Hitbox: constructeur par données... ";
+    Vec2f vecTest;
+    vecTest.x = 5.0;
+    vecTest.y = 10.0;
+    Hitbox h2(vecTest, 2.0, 4.0);
+    Vec2f vecTest2 = h2.getPos();
+    assert(vecTest == vecTest2);
+    float* h1 = nullptr;
+    h1 = new float[1];
+    float* w1 = nullptr;
+    w1 = new float[1];
+    h2.getDimensions(w1, h1);
+    assert(*h1 == h2.height && *w1 == h2.width);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: setters... ";
+    h.setPos(vecTest);
+    h.setHeight(3);
+    h.setWidth(5);
+    assert(h.pos == vecTest);
+    assert(h.height == 3 && h.width == 5);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: overlapping... ";
+    bool test = overlaping(h, h2); //doit retourner vrai
+    assert(test); 
+    Hitbox h3;
+    test = overlaping(h, h3); //doit retourner faux
+    assert(!test);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: upper... ";
+    h3 = h2.upper(0.5);
+    assert(h3.pos.x == 5 && h3.pos.y == 13);
+    assert(h3.height == 1 && h3.width == 2);
+    h3 = h2.upper(-0.5);
+    assert(h3.pos.x == 5 && h3.pos.y == 12);
+    assert(h3.height == 1 && h2.width == 2);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: lower... ";
+    h3 = h2.lower(0.5);
+    assert(h3.pos.x == 5 && h3.pos.y == 10);
+    assert(h3.height == 1 && h3.width == 2);
+    h3 = h2.lower(-0.5);
+    assert(h3.pos.x == 5 && h3.pos.y == 11);
+    assert(h3.height == 1 && h3.width == 2);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: right_inner... ";
+    h3 = h2.right_inner(0.5);
+    assert(h3.pos.x == 6.5 && h3.pos.y == 10);
+    assert(h3.height == 4 && h3.width == 0.5);
+    h3 = h2.right_inner(-0.5);
+    assert(h3.pos.x == 6 && h3.pos.y == 10);
+    assert(h3.height == 4 && h3.width == 0.5);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: left_inner... ";
+    h3 = h2.left_inner(0.5);
+    assert(h3.pos.x == 5 && h3.pos.y == 10);
+    assert(h3.height == 4 && h3.width == 0.5);
+    h3 = h2.left_inner(-0.5);
+    assert(h3.pos.x == 5.5 && h3.pos.y == 10);
+    assert(h3.height == 4 && h3.width == 0.5);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: top... ";
+    h3 = h2.top(2.5);
+    assert(h3.pos.x == 5 && h3.pos.y == 14);
+    assert(h3.height == 10 && h3.width == 2);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: bottom... ";
+    h3 = h2.bottom(2.5);
+    assert(h3.pos.x == 5 && h3.pos.y == 0);
+    assert(h3.height == 10 && h3.width == 2);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: right_outer... ";
+    h3 = h2.right_outer(2.5);
+    assert(h3.pos.x == 7 && h3.pos.y == 10);
+    assert(h3.height == 4 && h3.width == 5);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: left_outer... ";
+    h3 = h2.left_outer(2.5);
+    assert(h3.pos.x == 0 && h3.pos.y == 10);
+    assert(h3.height == 4 && h3.width == 5);
+    cout << "OK" << endl;
+
+    cout << "Hitbox: resized... ";
+    h3 = h2.resized(10);
+    assert(h3.pos.x == -4 && h3.pos.y == -8);
+    assert(h3.height == 40 && h3.width == 20);
+    h3 = h2.resized(0);
+    assert(h3.pos.x == 6 && h3.pos.y == 12);
+    assert(h3.height == 0 && h3.width == 0);
+    cout << "OK" << endl;
 }
