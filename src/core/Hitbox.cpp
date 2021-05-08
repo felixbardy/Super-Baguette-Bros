7e88501cc9c4d3bd57d79524fb932adf8fddee59
BEKKER VICTORIA p1914415 <victoria.bekker@etu.univ-lyon1.fr>
2021-05-08 13:58:00 +0000

    Ajout du test de regression et du constructeur par defaut

--- src/core/Hitbox.cpp
+++ src/core/Hitbox.cpp
@@ -1,324 +1,239 @@
-#include "Hitbox.h"
+#include "extern/vector2d.h"
 
-#include <iostream>
-#include <cassert>
-
-Hitbox::Hitbox()
-: width(0), height(0)
+/** \class Hitbox Hitbox.h "src/core/Hitbox.h"
+ * \brief représente une boite de collision
+ * 
+ * Une Hitbox a une position (Hitbox::getPos), une longueur et une largeur (Hitbox::getDimensions)
+ * \image html hitbox.jpg width=25%
+ * La classe implémente Hitbox::overlaping pour tester les collisions entre 2 Hitbox.
+ * Elle implémente également des getters et setters pour toutes ses données
+ * et de nombreuses méthodes pour séléctionner des sous parties ou les alentours de la Hitbox.<br>
+ * Ces dernières prennent un paramètre scale dont le fonctionnement est décris pour chaque méthode.<br><br>
+ * 
+ * Cette structure permet d'effectuer des détections de collisions plus fines tout en restant lisibles.<br>
+ * Exemples de détections:<br>
+ * <i>"La collision entre le dessous du joueur et le dessus de la plateforme"</i>
+ * ```cpp
+ * Hitbox player_hb, platform_hb;
+ * 
+ * if ( Hitbox::overlaping( player_hb.bottom(), platform_hb.top() ) )
+ *     //do something
+ * ```
+ * <i>"La collision entre la tête du joueur et le bas de la plateforme"</i>
+ * ```cpp
+ * Hitbox player_hb, platform_hb;
+ * 
+ * if ( Hitbox::overlaping( player_hb.upper(), platform_hb.lower() ) )
+ *     //do something
+ * ```
+ *
+ * Tour d'horizon des méthodes de modulation des hitboxes:
+ * \image html hitbox1.jpg width=25%
+ * \image html hitbox2.jpg width=25%
+ * \image html hitbox3.jpg width=25%
+ * 
+ **/
+class Hitbox
 {
-    pos.x = 0;
-    pos.y = 0;
-}
+private:
+    // La position de la Hitbox
+    Vec2f pos;
+    // La largeur de la Hitbox
+    float width;
+    // La hauteur de la Hitbox
+    float height;
+public:
 
-Hitbox::Hitbox(Vec2f pos, float width, float height)
-: pos(pos), width(width), height(height)
-{
-}
+    /**
+     * Constructeur par defaut
+     **/
+    Hitbox();
 
-Hitbox::~Hitbox()
-{
-}
+    /** \brief Constructeur par données de Hitbox
+     * \param pos la position de la Hitbox
+     * \param width la largeur de la Hitbox
+     * \param height le hauteur de la Hitbox
+     **/
+    Hitbox(Vec2f pos, float width, float height);
 
-
-Vec2f Hitbox::getPos() const { return pos; }
-
-void Hitbox::setPos(Vec2f newpos) { pos = newpos; }
-
-void Hitbox::getDimensions(float* width, float* height) const
-{
-    if (!(width == NULL)) *width = this->width;
-
-    if (!(height == NULL)) *height = this->height;
-}
-
-void Hitbox::setWidth(float newwidth)
-{
-    width = newwidth;
-}
-
-void Hitbox::setHeight(float newheight)
-{
-    height = newheight;
-}
-
-bool Hitbox::overlaping(const Hitbox& h1, const Hitbox& h2)
-{
-    return !( // Vrai sauf si:
-           ((h2.pos.x + h2.width) < h1.pos.x)  // h2 est complètement à gauche de h1
-        || ((h1.pos.x + h1.width) < h2.pos.x)  // h1 est complètement à gauche de h2
-        || ((h2.pos.y + h2.height) < h1.pos.y) // h2 est complètement en dessous de h1
-        || ((h1.pos.y + h1.height) < h2.pos.y) // h1 est complètement en dessous de h2
-    );
-}
-
-Hitbox Hitbox::upper(float scale) const
-{
-    assert(scale >= -1 && scale <= 1 && scale != 0);
-
-    if (scale > 0) // si scale est positif
-    return Hitbox(
-        Vec2f(
-            pos.x,
-            pos.y + (height / 2.0f) * (1+1-scale)
-        ),
-        width,
-        (height / 2.0f) * (scale)
-    );
-    else // si scale est négatif
-    return Hitbox(
-        Vec2f(
-            pos.x,
-            pos.y + (height / 2.0f)
-        ),
-        width,
-        (height / 2.0f) * (-scale)
-    );
-}
-
-Hitbox Hitbox::lower(float scale) const
-{
-    assert(scale >= -1 && scale <= 1 && scale != 0);
-
-    if (scale > 0) // si scale est positif
-    return Hitbox(
-        Vec2f(
-            pos.x,
-            pos.y
-        ),
-        width,
-        (height / 2.0f) * (scale)
-    );
-    else // si scale est négatif
-    return Hitbox(
-        Vec2f(
-            pos.x,
-            pos.y + (height / 2.0f) * (1 + scale) //? rappel: scale < 0
-        ),
-        width,
-        (height / 2.0f) * (-scale)
-    );
-}
-
-Hitbox Hitbox::right_inner(float scale) const
-{
-    assert(scale >= -1 && scale <= 1 && scale != 0);
-
-    if (scale > 0) // si scale est positif
-    return Hitbox(
-        Vec2f(
-            pos.x + (width / 2.0f) * (1 + 1 - scale),
-            pos.y
-        ),
-        (width / 2.0f) * (scale),
-        height
-    );
-    else // si scale est négatif
-    return Hitbox(
-        Vec2f(
-            pos.x + (width / 2.0f),
-            pos.y
-        ),
-        (width / 2.0f) * (-scale),
-        height
-    );
-}
-
-Hitbox Hitbox::left_inner(float scale) const
-{
-    assert(scale >= -1 && scale <= 1 && scale != 0);
-
-    if (scale > 0) // si scale est positif
-    return Hitbox(
-        Vec2f(
-            pos.x,
-            pos.y
-        ),
-        (width / 2.0f) * (scale),
-        height
-    );
-    else // si scale est négatif
-    return Hitbox(
-        Vec2f(
-            pos.x + (width / 2.0f) * (1 + scale), //? rappel: scale < 0
-            pos.y
-        ),
-        (width / 2.0f) * (-scale),
-        height
-    );
-}
+    /// Destructeur de Hitbox
+    ~Hitbox();
 
 
-Hitbox Hitbox::top(float scale) const
-{
-    assert(scale > 0);
-    return Hitbox(
-        Vec2f(
-            pos.x,
-            pos.y + height
-        ),
-        width,
-        height * scale
-    );
-}
+    /// Renvoie la position de la Hitbox
+    Vec2f getPos() const;
 
-Hitbox Hitbox::bottom(float scale) const
-{
-    assert(scale > 0);
-    return Hitbox(
-        Vec2f(
-            pos.x,
-            pos.y - height * scale
-        ),
-        width,
-        height * scale
-    );
-}
-
-Hitbox Hitbox::right_outer(float scale) const
-{
-    assert(scale > 0);
-    return Hitbox(
-        Vec2f(
-            pos.x + width,
-            pos.y
-        ),
-        width * scale,
-        height
-    );
-}
-
-Hitbox Hitbox::left_outer(float scale) const
-{
-    assert(scale > 0);
-    return Hitbox(
-        Vec2f(
-            pos.x - width * scale,
-            pos.y
-        ),
-        width * scale,
-        height
-    );
-}
-
-Hitbox Hitbox::resized(float scale) const
-{
-    assert(scale >= 0);
-    return Hitbox(
-        Vec2f(
-            pos.x + (1-scale) * width / 2.0f,
-            pos.y + (1-scale) * height / 2.0f
-        ),
-        width * scale,
-        height * scale
-    );
-}
-
-void Hitbox::testRegression()
-{
-    cout << "Hitbox: constructeur par défaut... ";
-    Hitbox h;
-    assert(h.pos.x == 0 && h.pos.y == 0);
-    assert(h.width == 0 && h.height == 0);
-    cout << "OK" << endl;
+    /// Affecte la nouvelle position à la Hitbox
+    void setPos(Vec2f newpos);
 
 
-    cout << "Hitbox: constructeur par données... ";
-    Vec2f vecTest;
-    vecTest.x = 5.0;
-    vecTest.y = 10.0;
-    Hitbox h2(vecTest, 2.0, 4.0);
-    Vec2f vecTest2 = h2.getPos();
-    assert(vecTest == vecTest2);
-    float* h1 = nullptr;
-    h1 = new float[1];
-    float* w1 = nullptr;
-    w1 = new float[1];
-    h2.getDimensions(w1, h1);
-    assert(*h1 == h2.height && *w1 == h2.width);
-    cout << "OK" << endl;
+    /** \brief Renvoie les dimensions de la Hitbox
+     * 
+     * width et height peuvent être NULL
+     * \param width le float auquel affecter la largeur
+     * \param heigth le float auquel affecter la hauteur
+     **/
+    void getDimensions(float* width, float* height) const;
 
-    cout << "Hitbox: setters... ";
-    h.setPos(vecTest);
-    h.setHeight(3);
-    h.setWidth(5);
-    assert(h.pos == vecTest);
-    assert(h.height == 3 && h.width == 5);
-    cout << "OK" << endl;
+    /** \brief Remplace la largeur de la Hitbox
+     * \param width la largeur à affecter
+     **/
+    void setWidth(float newwidth);
 
-    cout << "Hitbox: overlapping... ";
-    bool test = overlaping(h, h2); //doit retourner vrai
-    assert(test); 
-    Hitbox h3;
-    test = overlaping(h, h3); //doit retourner faux
-    assert(!test);
-    cout << "OK" << endl;
+    /** \brief Remplace la hauteur de la Hitbox
+     * \param width la hauteur à affecter
+     **/
+    void setHeight(float newheight);
 
-    cout << "Hitbox: upper... ";
-    h3 = h2.upper(0.5);
-    assert(h3.pos.x == 5 && h3.pos.y == 13);
-    assert(h3.height == 1 && h3.width == 2);
-    h3 = h2.upper(-0.5);
-    assert(h3.pos.x == 5 && h3.pos.y == 12);
-    assert(h3.height == 1 && h2.width == 2);
-    cout << "OK" << endl;
 
-    cout << "Hitbox: lower... ";
-    h3 = h2.lower(0.5);
-    assert(h3.pos.x == 5 && h3.pos.y == 10);
-    assert(h3.height == 1 && h3.width == 2);
-    h3 = h2.lower(-0.5);
-    assert(h3.pos.x == 5 && h3.pos.y == 11);
-    assert(h3.height == 1 && h3.width == 2);
-    cout << "OK" << endl;
+    /** \brief Renvoie true si les 2 hitboxs sont superposées, faux sinon
+     * \param h1 la 1ère Hitbox
+     * \param h2 la 2ème Hitbox
+     **/
+    static bool overlaping(const Hitbox& h1, const Hitbox& h2);
 
-    cout << "Hitbox: right_inner... ";
-    h3 = h2.right_inner(0.5);
-    assert(h3.pos.x == 6.5 && h3.pos.y == 10);
-    assert(h3.height == 4 && h3.width == 0.5);
-    h3 = h2.right_inner(-0.5);
-    assert(h3.pos.x == 6 && h3.pos.y == 10);
-    assert(h3.height == 4 && h3.width == 0.5);
-    cout << "OK" << endl;
 
-    cout << "Hitbox: left_inner... ";
-    h3 = h2.left_inner(0.5);
-    assert(h3.pos.x == 5 && h3.pos.y == 10);
-    assert(h3.height == 4 && h3.width == 0.5);
-    h3 = h2.left_inner(-0.5);
-    assert(h3.pos.x == 5.5 && h3.pos.y == 10);
-    assert(h3.height == 4 && h3.width == 0.5);
-    cout << "OK" << endl;
+    /** \brief Renvoie la partie intérieure haute de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [-1,1] \: \setminus \: 0 : \\\
+     * \begin{cases}
+     *     [-1,0[ \: :&      \left\vert scale \right\vert \text{ fois la partie haute en partant du milieu de la Hitbox} \\\
+     *     ]0,1] \: :&       scale \text{ fois la partie haute en partant du bord haut de la Hitbox} 
+     * \end{cases}
+     * \f]
+     * 
+     * 
+     * \image html hitbox_upper.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox upper(float scale = 1) const;
 
-    cout << "Hitbox: top... ";
-    h3 = h2.top(2.5);
-    assert(h3.pos.x == 5 && h3.pos.y == 14);
-    assert(h3.height == 10 && h3.width == 2);
-    cout << "OK" << endl;
+    /** \brief Renvoie la partie intérieure basse de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [-1,1] \: \setminus \: 0 : \\\
+     * \begin{cases}
+     *     [-1,0[ \: :&      \left\vert scale \right\vert \text{ fois la partie basse en partant du milieu de la Hitbox} \\\
+     *     ]0,1] \: :&       scale \text{ fois la partie basse en partant du bord bas de la Hitbox} 
+     * \end{cases}
+     * \f]
+     * 
+     * \image html hitbox_lower.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox lower(float scale = 1) const;
 
-    cout << "Hitbox: bottom... ";
-    h3 = h2.bottom(2.5);
-    assert(h3.pos.x == 5 && h3.pos.y == 0);
-    assert(h3.height == 10 && h3.width == 2);
-    cout << "OK" << endl;
+    /** \brief Renvoie la partie intérieure droite de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [-1,1] \: \setminus \: 0 : \\\
+     * \begin{cases}
+     *     [-1,0[ \: :&      \left\vert scale \right\vert \text{ fois la partie droite en partant du milieu de la Hitbox} \\\
+     *     ]0,1] \: :&       scale \text{ fois la partie droite en partant du bord droit de la Hitbox} 
+     * \end{cases}
+     * \f]
+     * 
+     * \image html hitbox_right_inner.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox right_inner(float scale = 1) const;
 
-    cout << "Hitbox: right_outer... ";
-    h3 = h2.right_outer(2.5);
-    assert(h3.pos.x == 7 && h3.pos.y == 10);
-    assert(h3.height == 4 && h3.width == 5);
-    cout << "OK" << endl;
+    /** \brief Renvoie la partie intérieure gauche de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [-1,1] \: \setminus \: 0 : \\\
+     * \begin{cases}
+     *     [-1,0[ \: :&      \left\vert scale \right\vert \text{ fois la partie gauche en partant du milieu de la Hitbox} \\\
+     *     ]0,1] \: :&       scale \text{ fois la partie gauche en partant du bord gauche de la Hitbox} 
+     * \end{cases}
+     * \f]
+     * 
+     * \image html hitbox_left_inner.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox left_inner(float scale = 1) const;
 
-    cout << "Hitbox: left_outer... ";
-    h3 = h2.left_outer(2.5);
-    assert(h3.pos.x == 0 && h3.pos.y == 10);
-    assert(h3.height == 4 && h3.width == 5);
-    cout << "OK" << endl;
 
-    cout << "Hitbox: resized... ";
-    h3 = h2.resized(10);
-    assert(h3.pos.x == -4 && h3.pos.y == -8);
-    assert(h3.height == 40 && h3.width == 20);
-    h3 = h2.resized(0);
-    assert(h3.pos.x == 6 && h3.pos.y == 12);
-    assert(h3.height == 0 && h3.width == 0);
-    cout << "OK" << endl;
-}
+    /** \brief Renvoie la partie extérieure haute de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [0,+\infty[ \: : \\\
+     * \text{Renvoie une Hitbox de la même largeur et de hauteur }
+     *  scale \text{ fois la hauteur initiale située juste au dessus de la Hitbox initiale.}
+     * \f]
+     * 
+     * \image html hitbox_top.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox top(float scale = 0.25) const;
+
+    /** \brief Renvoie la partie extérieure basse de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [0,+\infty[ \: : \\\
+     * \text{Renvoie une Hitbox de la même largeur et de hauteur }
+     *  scale \text{ fois la hauteur initiale située juste au dessous de la Hitbox initiale.}
+     * \f]
+     * 
+     * \image html hitbox_bottom.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox bottom(float scale = 0.25) const;
+
+    /** \brief Renvoie la partie extérieure droite de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [0,+\infty[ \: : \\\
+     * \text{Renvoie une Hitbox de la même hauteur et de largeur }
+     *  scale \text{ fois la largeur initiale située juste à droite de la Hitbox initiale.}
+     * \f]
+     * 
+     * \image html hitbox_right_outer.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox right_outer(float scale = 0.25) const;
+
+    /** \brief Renvoie la partie extérieure gauche de la Hitbox (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [0,+\infty[ \: : \\\
+     * \text{Renvoie une Hitbox de la même hauteur et de largeur }
+     *  scale \text{ fois la largeur initiale située juste à gauche de la Hitbox initiale.}
+     * \f]
+     * 
+     * \image html hitbox_left_outer.jpg width=25%
+     * 
+     * \param scale paramètre de modulation
+     **/
+    Hitbox left_outer(float scale = 0.25) const;
+
+
+    /** \brief Renvoie la Hitbox redimensionnée (modulée par scale)
+     * 
+     * \f[
+     * \text{scale} \in [0,+\infty[ \: : \\\
+     * \text{Renvoie une Hitbox de même centre avec des dimension de }
+     *  scale \text{ fois les dimensions initiales.}
+     * \f]
+     * 
+     * \image html hitbox_resized.jpg width=25%
+     * 
+     * \param scale facteur de redimensionnement
+     **/
+    Hitbox resized(float scale) const;
+
+    /**
+     * Test de regression de Hitbox.
+    */
+    void testRegression();
+
+};
+
