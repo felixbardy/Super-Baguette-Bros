2212ad25874b52e05d68e9b57a2f145b2ca31a4c
BEKKER VICTORIA p1914415 <victoria.bekker@etu.univ-lyon1.fr>
2021-05-10 14:41:20 +0000

    Petite modification de la partie score du test de regression

--- src/core/World.cpp
+++ src/core/World.cpp
@@ -469,13 +469,13 @@ void World::testRegression()
     //TODO Implémenter le test du constructeur par fichier
     cout << "World: constructeur par fichier... " ;
 
-    /*World w3("data/levels/example_level.xml");
+    World w3("data/levels/example_level.xml");
     assert(w3.score == 0);
     assert(w3.frameCount == 0);
     assert(w3.getPlatforms() != nullptr);
     assert(w3.getPlatformsSizes() != nullptr);
     assert(w3.segments != nullptr);
-    assert(w3.nSegments == 6);*/
+    assert(w3.nSegments == 6);
     
 
     cout << "WIP" << endl;
@@ -504,13 +504,13 @@ void World::testRegression()
     cout << "WIP" << endl;
 
     cout << "World: step...";
-    //TODO test regression de step
+    //assert(w1.step() == 0);
     cout << "WIP" << endl;
 
     cout << "World: des autres getters...";
-    assert(getScore() == 0);
-    score++;
-    assert(getScore() == 1);
+    assert(w1.getScore() == 0);
+    w1.score++;
+    assert(w1.getScore() == 1);
     w2.segmentWidth = 1;
     assert(w2.getWorldEnd() == w2.nSegments * w2.segmentWidth);
     cout << "OK" << endl;
